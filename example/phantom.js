4acb8c53df8d0b603239628a191b7eaa2e4c2663
alex-seville <alexander.southern@gmail.com>
2013-02-20 08:05:20 -0500

    fixed

--- example/phantom.js
+++ example/phantom.js
@@ -1,16 +1,18 @@
 var page = require('webpage').create(),
 	system = require('system');
-var url  = system.args[1];
+var url  = system.args[2];
+var testfile = system.args[1];
+var delimiter = "FETA:";
 
 page.onConsoleMessage = function (msg) {
-    if (msg.slice(0,3) === "***"){
-        console.log(msg);
+    if (msg.indexOf(delimiter) === 0){
+        console.log(msg.slice(delimiter.length));
         phantom.exit();
     }
 };
 
 page.open(url, function (status) {
-    page.includeJs("http://localhost:3000/example/test.js", function() {
+    page.includeJs(testfile, function() {
         //Page is loaded!
         //phantom.exit();
     });
--- example/test.js
+++ example/test.js
@@ -1,16 +1,57 @@
 (function($){
-$('HTML BODY DIV.main ARTICLE DIV#designing.designing DIV.designing-content-wrap DIV.designing-content UL.design-nav LI A.one.processSliderLinkX SPAN.navIndex').trigger('click');
-setTimeout(function(){ $('HTML BODY DIV.main ARTICLE DIV#designing.designing DIV.designing-content-wrap DIV.takeover-container DIV.takeover-content DIV.processSlider-wrapper.clearfix UL.processSlider NAV.processSlider-btns SPAN.processSlider-btn.processSlider-btnNext').trigger('click');
- } ,1638);
-setTimeout(function(){ $('HTML BODY DIV.main ARTICLE DIV#designing.designing DIV.designing-content-wrap DIV.takeover-container A.takeover-closeButton').trigger('click');
- } ,2583);
-setTimeout(function(){
-	//Add your verification code here
-	var opacNum = parseFloat($('HTML BODY DIV.main ARTICLE DIV#designing.designing DIV.designing-content-wrap DIV.takeover-container .takeover-content').css("opacity"));
-	if (opacNum < 1){
-		console.log("***No Regression");
-	}else{
-		console.log("***Regression! Opacity:",opacNum);
-	}
-},2600);
+function regression(){
+var regressions=[];
+return {
+add: function(item){
+item = item || '';
+regressions.push(item);
+},
+checkIfRegressed: function(cb){
+var res = regressions.length > 0;
+if (cb){
+cb(res,regressions);
+}else{
+if (res ){
+console.log('FETA:Regression!');
+}else{
+console.log('FETA:No Regression.');
+}
+}
+}
+};
+}
+var r = new regression();
+$('HTML BODY DIV.main ARTICLE DIV#designing.designing DIV.designing-content-wrap DIV.designing-content UL.design-nav LI A.one.processSliderLinkX SPAN.slideshow-highlight').trigger('click');
+//add any verification code here
+//use `r.add()` 
+setTimeout(function(){ $('HTML BODY DIV.main ARTICLE DIV#designing.designing DIV.designing-content-wrap DIV.takeover-container.s-shown DIV.takeover-content DIV.processSlider-wrapper.clearfix UL.processSlider.readyForMenuAni NAV.processSlider-btns SPAN.processSlider-btn.processSlider-btnNext').trigger('click');
+//add any verification code here
+//use `r.add()` 
+ } ,2908);
+setTimeout(function(){ $('HTML BODY DIV.main ARTICLE DIV#designing.designing DIV.designing-content-wrap DIV.takeover-container.s-shown DIV.takeover-content DIV.processSlider-wrapper.clearfix UL.processSlider.readyForMenuAni NAV.processSlider-btns SPAN.processSlider-btn.processSlider-btnNext').trigger('click');
+//add any verification code here
+//use `r.add()` 
+ } ,3974);
+setTimeout(function(){ $('HTML BODY DIV.main ARTICLE DIV#designing.designing DIV.designing-content-wrap DIV.takeover-container.s-shown DIV.takeover-content DIV.processSlider-wrapper.clearfix UL.processSlider.readyForMenuAni NAV.processSlider-btns SPAN.processSlider-btn.processSlider-btnNext').trigger('click');
+//add any verification code here
+//use `r.add()` 
+ } ,5010);
+setTimeout(function(){ $('HTML BODY DIV.main ARTICLE DIV#designing.designing DIV.designing-content-wrap DIV.takeover-container.s-shown DIV.takeover-content DIV.processSlider-wrapper.clearfix UL.processSlider.readyForMenuAni NAV.processSlider-btns SPAN.processSlider-btn.processSlider-btnNext').trigger('click');
+//add any verification code here
+
+//use `r.add()` 
+ } ,5893);
+setTimeout(function(){ 
+if (!$('HTML BODY DIV.main ARTICLE DIV#designing.designing DIV.designing-content-wrap DIV.takeover-container.s-shown DIV.takeover-content DIV.processSlider-wrapper.clearfix UL.processSlider.readyForMenuAni NAV.processSlider-btns SPAN.processSlider-btn.processSlider-btnNext').hasClass('s-disabled')){
+    r.add();
+}
+    $('HTML BODY DIV.main ARTICLE DIV#designing.designing DIV.designing-content-wrap DIV.takeover-container A.takeover-closeButton').trigger('click');
+//add any verification code here
+//use `r.add()` 
+ } ,7334);
+setTimeout(function(){ 
+//Add your verification code here
+
+r.checkIfRegressed();
+},7434);
 })(jQuery);
