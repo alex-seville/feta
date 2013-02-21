e800475eebfed01d51a2290b673b061d9c4e28e1
alex-seville <alexander.southern@gmail.com>
2013-02-21 08:02:55 -0500

    Added failing test for sibling selector

--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,18 @@
-test( "hello test", function() {
-  ok( 1 == "1", "Passed!" );
+test( "sibling selector", function() {
+    expect(2);
+  feta.start();
+  $("#siblingTest ul li").eq(2).trigger("click");
+  feta.stop(function(results){
+    ok( results.events.length === 1, "Correct number of events." );
+    $("#siblingTest ul li").eq(0).click(function(){
+        ok(false,"should not fire this");
+    });
+    $("#siblingTest ul li").eq(2).click(function(){
+        ok(false,"should not fire this");
+    });
+    $("#siblingTest ul li").eq(1).click(function(){
+        ok(true,"should fire this");
+    });
+    eval(results.JS);
+  });
 });
